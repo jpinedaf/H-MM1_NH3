b69e0427d3816d2132c5b1db194711968d27da2c
Jaime Pineda <jaime.e.pineda@gmail.com>
2021-04-23 11:07:44 +0200

    Reorganisation -added config file -added file with combination of optically thin and thick fits

---
+++ config.py
@@ -0,0 +1,38 @@
+file_NH3_thick = 'fits/H-MM1_NH3_fit_thick_par_snr5.fits'
+file_NH3_thin = 'fits/H-MM1_NH3_fit_thin_par_snr5.fits'
+file_oNH2D_thin = 'fits/H-MM1_oNH2D_fit_thin_par_snr5.fits'
+file_oNH2D_thick = 'fits/H-MM1_oNH2D_fit_thick_par_snr5.fits'
+file_pNH2D_thin = 'fits/H-MM1_pNH2D_fit_thin_par_snr5.fits'
+file_pNH2D_thick = 'fits/H-MM1_pNH2D_fit_thick_par_snr5.fits'
+
+file_oNH2D_Tex = 'fits/H-MM1_oNH2D_Tex_snr5.fits'
+file_oNH2D_eTex = 'fits/H-MM1_oNH2D_eTex_snr5.fits'
+file_oNH2D_tau = 'fits/H-MM1_oNH2D_tau_snr5.fits'
+file_oNH2D_etau = 'fits/H-MM1_oNH2D_etau_snr5.fits'
+file_oNH2D_Vlsr = 'fits/H-MM1_oNH2D_Vlsr_snr5.fits'
+file_oNH2D_eVlsr = 'fits/H-MM1_oNH2D_eVlsr_snr5.fits'
+file_oNH2D_dv = 'fits/H-MM1_oNH2D_sigma_v_snr5.fits'
+file_oNH2D_edv = 'fits/H-MM1_oNH2D_esigma_v_snr5.fits'
+
+file_pNH2D_Tex = 'fits/H-MM1_pNH2D_Tex_snr5.fits'
+file_pNH2D_eTex = 'fits/H-MM1_pNH2D_eTex_snr5.fits'
+file_pNH2D_tau = 'fits/H-MM1_pNH2D_tau_snr5.fits'
+file_pNH2D_etau = 'fits/H-MM1_pNH2D_etau_snr5.fits'
+file_pNH2D_Vlsr = 'fits/H-MM1_pNH2D_Vlsr_snr5.fits'
+file_pNH2D_eVlsr = 'fits/H-MM1_pNH2D_eVlsr_snr5.fits'
+file_pNH2D_dv = 'fits/H-MM1_pNH2D_sigma_v_snr5.fits'
+file_pNH2D_edv = 'fits/H-MM1_pNH2D_esigma_v_snr5.fits'
+
+file_NH3_Tex = 'fits/H-MM1_NH3_Tex_snr5.fits'
+file_NH3_eTex = 'fits/H-MM1_NH3_eTex_snr5.fits'
+file_NH3_Tkin = 'fits/H-MM1_NH3_Tkin_snr5.fits'
+file_NH3_eTkin = 'fits/H-MM1_NH3_eTkin_snr5.fits'
+file_NH3_Vlsr = 'fits/H-MM1_NH3_Vlsr_snr5.fits'
+file_NH3_eVlsr = 'fits/H-MM1_NH3_eVlsr_snr5.fits'
+file_NH3_dv = 'fits/H-MM1_NH3_sigma_v_snr5.fits'
+file_NH3_edv = 'fits/H-MM1_NH3_esigma_v_snr5.fits'
+file_NH3_Ncol = 'fits/H-MM1_NH3_Ncol_snr5.fits'
+file_NH3_eNcol = 'fits/H-MM1_NH3_eNcol_snr5.fits'
+
+# Mean value from optically thick fit, used in optically thin
+Tkin_mean = 11.0  # K
